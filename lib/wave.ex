3305066f5ce03819dd38818aaac6ef38a593c809
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-10-30 00:07:54 +0100

    stub initial tests

--- lib/wave.ex
+++ lib/wave.ex
@@ -1,18 +1,29 @@
 defmodule Wave do
   @moduledoc """
   Documentation for `Wave`.
+
+  Operations:
+
+  1. create a ledger (only one provided by default)
+  2. create more than one account on the ledger
+  3. add a CR entry
+  4. add a DR entry
+  5. retrieve a balance
   """
 
-  @doc """
-  Hello world.
+  def create_account(_ledger_type) do
+    nil
+  end
 
-  ## Examples
+  def create_credit_entry(_cr_account_id, _dr_account_id, _amount) do
+    nil
+  end
 
-      iex> Wave.hello()
-      :world
+  def create_debit_entry(_cr_account_id, _dr_account_id, _amount) do
+    nil
+  end
 
-  """
-  def hello do
-    :world
+  def account_balance(_account_id) do
+    nil
   end
 end
--- test/wave_test.exs
+++ test/wave_test.exs
@@ -2,7 +2,27 @@ defmodule WaveTest do
   use ExUnit.Case
   doctest Wave
 
-  test "greets the world" do
-    assert Wave.hello() == :world
+  describe "account_creation" do
+    test "it creates an account on a ledger" do
+      assert Wave.create_account(:accounts_payable) == nil
+    end
+
+    test "it creates more than one account on a ledger" do
+      assert Wave.create_account(:accounts_recievable) == nil
+    end
+  end
+
+  describe "transactions" do
+    test "it creates a balanced CR/DR entry" do
+      assert Wave.create_credit_entry("test", "test-two", 5000) == nil
+    end
+
+    test "it creates a balanced DR/CR entry" do
+      assert Wave.create_debit_entry("test", "test-two", 5000) == nil
+    end
+
+    test "it retrieves a balance" do
+      assert Wave.account_balance("test") == nil
+    end
   end
 end
